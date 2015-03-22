93a060cc438c769cc18f45f3eddbf496fa82e6bf
Gaelan D'costa <gdcosta@gmail.com>
2015-03-21 20:12:53 -0400

    Initial Commit

---
+++ Reserve a Server/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reserve a Server
+//
+//  Created by Gaelan D'costa on 2015-03-21.
+//  Copyright (c) 2015 Gaelan D'costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
